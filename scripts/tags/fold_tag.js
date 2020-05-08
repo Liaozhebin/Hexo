fc5a85c0bd4bca4fca1ae910dcd9540aaf957447
Liaozhebin <liaozhebin@gmail.com>
2020-05-08 18:04:06 +0800

    update 合并并迁移文件到hexo数据文件目录，删除 fold.js

--- scripts/tags/fold_tag.js
+++
@@ -1,8 +0,0 @@
-/* global hexo */
-// Usage: {% fold 点击显/隐 %} Something {% endfold %}
-function fold (args, content) {
-  var text = args[0];
-  if(!text) text = "点击显/隐内容";
-  return '<div><div class="fold_hider"><div class="close hider_title">' + text + '</div></div><div class="fold">\n' + hexo.render.renderSync({text: content, engine: 'markdown'}) + '\n</div></div>';
-}
-hexo.extend.tag.register('fold', fold, {ends: true});
